2529a1c11792a3304433be36d3c2dad2a4523db8
1nTh35h3ll <yossis@protonmail.com>
2025-07-23 16:22:53 +0300

    v1.0.5 - Added check for Forest DN rather than current domain + check for relevant deleted objects before moving on + ignore lack of tombstonelifetime value + some minor error handling

--- Get-DCShadowNTDSdsa.ps1
+++ Get-DCShadowNTDSdsa.ps1
@@ -1,31 +1,43 @@
-# comments to yossis@protonmail.com (v1.0.4) 
+# comments to yossis@protonmail.com (v1.0.5) 
+# v1.0.5 - Added check for Forest DN rather than current domain + check for relevant deleted objects before moving on + ignore lack of tombstonelifetime value + some minor error handling
 # v1.0.4 - minor addition to reflect the number of Days Since Recycle Bin was Enabled
 # v1.0.3 - added checks for tombstoneLifetime & AD-RecycleBin enabled date + solved minor error in 'else' loop (not affecting the results but still)
 # v1.0.2 - added option to query other domains
 param (
-    [string]$DomainDNSname
+    # If other trusted domain specified as a parameter - use it. Otherwise, use currently logged on *FOREST*
+    [string]$DomainDNSname = (Get-ADRootDSE).rootDomainNamingContext.Replace("DC=","").Replace(",",".")
 )
 
-if ($DomainDNSname -eq "") {$DomainDNSname = $env:USERDNSDOMAIN}
-
-$recycleBinEnabled = Get-ADOptionalFeature -Filter {Name -like "Recycle Bin Feature"} -Server $DomainDNSname | Select-Object -ExpandProperty EnabledScopes
+$recycleBinEnabled = Get-ADOptionalFeature -Filter {Name -like "Recycle Bin Feature"} -Server $DomainDNSname -ErrorAction SilentlyContinue | Select-Object -ExpandProperty EnabledScopes
+if (!$?)
+{
+    Write-Host "[!] The domain $DomainDNSname could not be contacted.`n$($Error[0].exception.message)`nQuiting." -ForegroundColor Yellow;
+    break
+}
 
 if ($recycleBinEnabled) {
-    Write-Host "Active Directory Recycle Bin is enabled in the domain.`nContinuing to check for DC demotion / DC-Shadow exploitation..." -ForegroundColor Green
+    Write-Host "[x] Active Directory Recycle Bin is enabled in the domain.`nContinuing to check for DC demotion / DC-Shadow exploitation..." -ForegroundColor Green
 } else {
-    Write-Host "Active Directory Recycle Bin is not enabled in the domain. Quiting." -ForegroundColor Yellow;
-    exit
+    Write-Host "[!] Active Directory Recycle Bin is not enabled in the domain. Quiting." -ForegroundColor Yellow;
+    break
 }
 
 $root = Get-ADRootDSE -Server $DomainDNSname;
 $deletedFromCN = Get-ADObject -ResultPageSize 100000 -searchbase $root.configurationNamingContext -Server $DomainDNSname -filter {(IsDeleted -eq $true) -and (ObjectClass -ne "msExchActiveSyncDevice")} -IncludeDeletedObjects -properties *;
 $nTDSDSAResult = $deletedFromCN | where-object {$_.ObjectClass -eq 'nTDSDSA'}
 
+# Ensure we got potentially relevant results from Deleted objects
+if (!$nTDSDSAResult) {
+    Write-Host "[!] No relevant entries found in the Recycle Bin. Quiting." -ForegroundColor Yellow;
+}
+
 # Check to see when AD recycle bin was enabled, and what is the tombstoneLifetime
 # Get tombstoneLifetime value in days
-$DomainDN = (Get-ADDomain -Server $DomainDNSname).DistinguishedName;
-$tombstoneLifetime = $((Get-ADObject -SearchBase "CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,$DomainDN" -Filter * -Property tombstoneLifetime).tombstoneLifetime | Out-String).Trim();
-Write-Host "Accessing domain $DomainDN <tombstoneLifetime = $tombstoneLifetime days>";
+#$DomainDN = (Get-ADDomain -Server $DomainDNSname).DistinguishedName;
+$tombstoneLifetime = $((Get-ADObject -SearchBase "CN=Directory Service,CN=Windows NT,CN=Services,$($root.configurationNamingContext)" -Filter * -Property tombstoneLifetime).tombstoneLifetime | Out-String).Trim();
+if ($tombstoneLifetime) {
+    Write-Host "[x] Accessing $DomainDNSname <tombstoneLifetime = $tombstoneLifetime days>"
+}
 
 # Get the date when AD Recycle Bin was enabled
 $configNCDN = (Get-ADRootDSE -Server $DomainDNSname).configurationNamingContext;
@@ -35,11 +47,13 @@ $directoryServiceDN = "CN=Directory Service,CN=Windows NT,CN=Services,$configNCD
 $directoryService = Get-ADObject -Identity $directoryServiceDN -Server $DomainDNSname -Properties whenChanged;
 $recycleBinEnabledDate = $directoryService.whenChanged;
 $DaysSinceRecycleBinEnabled = $($(get-date) - $recycleBinEnabledDate).Days;
-Write-Host "Recycle Bin was enabled on $recycleBinEnabledDate ($DaysSinceRecycleBinEnabled days ago)";
+Write-Host "[x] Recycle Bin was enabled on $recycleBinEnabledDate ($DaysSinceRecycleBinEnabled days ago)";
 
-$daysDiff = ($(Get-Date) - $recycleBinEnabledDate).Days;
-if ($daysDiff -lt $tombstoneLifetime) {$DaysBack = $tombstoneLifetime - $daysDiff} else {$DaysBack = $tombstoneLifetime}
-Write-Host "Results reflecting the last $daysback days (according to tombstoneLifetime & recycleBin Enabled date)`n" -ForegroundColor Cyan;
+if ($tombstoneLifetime) {
+    $daysDiff = ($(Get-Date) - $recycleBinEnabledDate).Days;
+    if ($daysDiff -lt $tombstoneLifetime) {$DaysBack = $tombstoneLifetime - $daysDiff} else {$DaysBack = $tombstoneLifetime}
+    Write-Host "[x] Results reflecting the last $daysback days (according to tombstoneLifetime & recycleBin Enabled date)`n" -ForegroundColor Cyan
+}
 
 if ($nTDSDSAResult)
 	{        
@@ -52,7 +66,7 @@ if ($nTDSDSAResult)
             Write-Output "Entry $i of $nTDSDSAEntriesCount"
             if (($($nTDSDSA.whenCreated) -  $($nTDSDSA.whenChanged)).Hours -lt 1)
                 {
-                    Write-Host "HIGHLY SUSPICIOUS ENTRY -- Check Details Carefully!" -ForegroundColor Yellow
+                    Write-Host "[!] SUSPICIOUS ENTRY (" -ForegroundColor Yellow -NoNewline; Write-Host $DeletedObjectHostName -NoNewline -ForegroundColor Cyan; Write-Host ") -- Check Details Carefully!" -ForegroundColor Yellow
                 }
 
             Write-Output "Full object distinguished name: $($nTDSDSA.DistinguishedName)"
@@ -73,5 +87,5 @@ if ($nTDSDSAResult)
     }
 else
     {
-        Write-Host "No relevant entries found (no direct evidence of exploitation)." -ForegroundColor Green;
+        Write-Host "[x] No relevant entries found (no direct evidence of exploitation)." -ForegroundColor Green;
     }
